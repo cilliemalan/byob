61783d13e72bee35ef341ffe00275ee00a995f05
Cillié Malan <cilliemalan@gmail.com>
2018-03-16 16:46:44 +0200

    Added a few methods to check validity

--- utils.js
+++ utils.js
@@ -137,6 +137,53 @@ const getPublicKeyFromPrivateKey = (key) => {
     return Buffer.from(pub.encode('array', true));
 }
 
+/**
+ * Checks if a string is valid base64.
+ * @param {*} s the string to check.
+ * @returns {boolean} true or false.
+ */
+const isValidBase64 = (s) => {
+    return typeof s == "string" && (s == "" || /[-a-zA-Z0-9_+/]/.test(s));
+}
+
+/**
+ * Checks if something is a valid public key.
+ * @param {*} s the thing to check.
+ * @returns {boolean} true or false.
+ */
+const isValidPublicKey = (key) => {
+    if(typeof key == "string") {
+        return key.length == 44 && key[0] == 'A' &&
+            isValidBase64(key) && isValidPublicKey(decode(key));
+    } else if(key instanceof Buffer) {
+        return key.length == 33 && (key[0] == 2 || key[0] == 3);
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Checks if something is a valid private key.
+ * @param {*} s the thing to check.
+ * @returns {boolean} true or false.
+ */
+const isValidPrivateKey = (key) => {
+    if(typeof key == "string") {
+        return key.length == 43 && 
+        isValidBase64(key) && isValidPrivateKey(decode(key));
+    } else if(key instanceof Buffer) {
+        return key.length == 32;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Checks if something is a valid private key.
+ * @param {*} s the thing to check.
+ * @returns {boolean} true or false.
+ */
+const isValidHash = (hash) => isValidPrivateKey(hash);
 
 module.exports = {
     encode,
@@ -147,5 +194,10 @@ module.exports = {
     verify_sig,
     verify,
     generateKey,
-    getPublicKeyFromPrivateKey
+    getPublicKeyFromPrivateKey,
+
+    isValidBase64,
+    isValidPublicKey,
+    isValidPrivateKey,
+    isValidHash
 };

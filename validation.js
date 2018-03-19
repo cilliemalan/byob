5e3a15b0e188873aa91eeb8081ee16984ec302ca
Cillié Malan <cilliemalan@gmail.com>
2018-03-19 22:32:49 +0200

    Added validate_split

--- validation.js
+++ validation.js
@@ -1,4 +1,4 @@
-const { decode } = require('./utils');
+const { decode, verify } = require('./utils');
 
 
 /**
@@ -49,11 +49,29 @@ const is_valid_private_key = (key) => {
  */
 const is_valid_hash = (hash) => is_valid_private_key(hash);
 
+/**
+ * Checks if something is a valid split
+ * @param {*} split 
+ */
+const validate_split = (split) => {
+    const errors = [];
+    const { account, amount, ...rest } = split;
+    const restKeys = rest && Object.keys(rest);
+    if (!account) errors.push("account is required");
+    if (!amount) errors.push("An amount is required and cannot be zero");
+    if (amount !== 0 && amount > -1e-15 && amount < 1e-15) errors.push("Amounts must be greater than 1E-15");
+    if (account && !is_valid_public_key(account)) errors.push("account is not a valid public key");
+    if (restKeys && restKeys.length) errors.push(`The split contained extra unsupported properties: ${restKeys.join(", ")}`);
+
+    return errors;
+}
+
 
 
 module.exports = {
     is_valid_base64,
     is_valid_public_key,
     is_valid_private_key,
-    is_valid_hash
+    is_valid_hash,
+    validate_split
 };

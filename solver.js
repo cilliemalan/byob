a5dd7c7be394f88ad97c49e23ffea6e09ea122ad
Cillié Malan <cilliemalan@gmail.com>
2018-03-22 15:02:54 +0200

    Added solver which solves the hash problem for PoW

---
+++ solver.js
@@ -0,0 +1,55 @@
+const { decode, encode } = require('./utils');
+const { fork } = require('child_process');
+const { resolve: resolvePath } = require('path');
+
+const node = process.argv0;
+const worker = resolvePath(__dirname, 'solver.worker.js');
+let running_process;
+
+const runSolver = (hash, target) => {
+    if (running_process) {
+        running_process.kill('SIGINT');
+        running_process = null;
+    }
+
+    return new Promise((resolve, reject) => {
+        let solution;
+        running_process = fork(worker, [encode(hash), encode(target)]);
+
+        running_process.on('message', m => {
+            solution = m && m.nonce;
+        });
+
+        running_process.on('close', (code, sig) => {
+            if (code == 0) {
+                if (!solution) {
+                    reject('Child process exited without providing a solution');
+                } else {
+                    resolve(solution);
+                }
+            } else {
+                reject(`The child process exited with code ${code}`);
+            }
+        })
+    });
+}
+
+
+/**
+ * Finds a nonce such that hash ^ nonce < target. Note: if this is called while
+ * a previous problem is still in the process of being solved, that problem solving
+ * process will be cancelled and the previously returned promise will reject.
+ * @param {Buffer} hash The hash part of the problem
+ * @param {Buffer} target The target under which the solution should be
+ * @returns {Promise<Buffer>} the solution to the problem
+ */
+const solve = (hash, target) => {
+    if (typeof hash == "string") hash = decode(hash);
+    if (typeof target == "string") target = decode(target);
+
+    return runSolver(hash, target);
+}
+
+module.exports = {
+    solve
+}
---
+++ solver.worker.js
@@ -0,0 +1,79 @@
+const { encode, decode } = require('./utils');
+const { buffer_less_than } = require('./validation');
+const { randomBytes, createHash } = require('crypto');
+
+// some validation
+const shash = process.argv[2];
+const starget = process.argv[3];
+
+if (!shash) {
+    console.error('no hash was provided');
+    process.exit(5);
+}
+if (!starget) {
+    console.error('no target was provided');
+    process.exit(6);
+}
+
+const bhash = decode(shash);
+const btarget = decode(starget);
+
+if (bhash.length != 32) {
+    console.error(`the hash was not valid. Needs to be 32 bytes long. was ${bhash.length} bytes long.`);
+    process.exit(7);
+}
+if (btarget.length != 32) {
+    console.error(`the target was not valid. Needs to be 32 bytes long. was ${btarget.length} bytes long.`);
+    process.exit(8);
+}
+
+
+// does buffer1 ^ buffer2
+const compliment = (a, b) => {
+
+    const c = new Buffer(32);
+    for (let i = 0; i < 32; ++i) {
+        c[i] = a[i] ^ b[i];
+    }
+
+    return c;
+}
+
+// gets time now in seconds, but like in high resolution.
+const gethrnow = () => {
+    const tm = process.hrtime();
+    return tm[0] + tm[1] / 1000000000;
+}
+
+// some stuffs
+const REPORT_FREQ = 100000;
+let hashes_done = 0;
+let hrt = gethrnow();
+let hrs = gethrnow();
+
+// here we go!
+console.log(`mining...\n`)
+for (; ; ++hashes_done) {
+    const nonce = randomBytes(32);
+    const compl = compliment(nonce, bhash);
+    const digest = createHash('sha256').update(compl).digest();
+
+    if (buffer_less_than(digest, btarget)) {
+        console.log(`Found solution after ${parseInt(hashes_done / 10000) / 100} million hashes taking ${parseInt(gethrnow() - hrs)} seconds`);
+        console.log(`Nonce:         ${encode(nonce)}`);
+        console.log(`Hash:          ${encode(bhash)}`);
+        console.log(`Target:        ${encode(btarget)}`);
+        console.log(`Hased Nonce:   ${encode(digest)}`);
+        if (process.send) process.send({ nonce: encode(nonce) });
+        break;
+    }
+
+    // print a status report
+    if (hashes_done && (hashes_done % REPORT_FREQ == 0)) {
+        const hrnow = gethrnow();
+        const diff = hrnow - hrt;
+        const s = REPORT_FREQ / diff;
+        hrt = hrnow;
+        process.stdout.write(`hashrate: ${parseInt(s / 100) / 10} KH/s      \r`);
+    }
+}

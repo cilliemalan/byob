3f89c9f2f2058f44114b06e92637c62896a3b867
Cillié Malan <cilliemalan@gmail.com>
2018-04-04 11:25:28 +0200

    Added a bunch of solver test cases to see how quickly it recovers.

--- tests/solver.js
+++ tests/solver.js
@@ -3,7 +3,10 @@ const { ok, equal, notEqual, deepEqual } = require('assert');
 const { generate_nonce, calculate_target } = require('../utils');
 const { solve } = require('../solver');
 
+
 module.exports = {
-    'solver can solve an easy problem quickly': async () => ok(await solve(generate_nonce(), calculate_target(1000, 1))),
+    'solver can solve an easy problem quickly 1': async () => ok(await solve(generate_nonce(), calculate_target(1000, 1))),
+    'solver can solve an easy problem quickly 2': async () => ok(await solve(generate_nonce(), calculate_target(1000, 1))),
+    'solver can solve an easy problem quickly 3': async () => ok(await solve(generate_nonce(), calculate_target(1000, 1))),
     'solver can solve an not-so-easy problem': async () => ok(await solve(generate_nonce(), calculate_target(1000, 1000))),
 }

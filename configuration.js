0fb4e35fc594a2240cbc0d81f68d7d76e18d22ce
Cillié Malan <cilliemalan@gmail.com>
2018-06-06 22:03:53 +0200

    Fixed configuration error

--- configuration.js
+++ configuration.js
@@ -7,5 +7,5 @@ module.exports = {
         resolve(process.env.USERPROFILE || process.env.HOME, '.byobkeys.json')),
     DB_FILE: resolve(process.env.BYOB_DB_FILE ||
         resolve(process.cwd(), 'db.json')),
-    BROKER: BYOB_BROKER || "amqps://byob:byob@mq.chills.co.za/byob"
+    BROKER: process.env.BYOB_BROKER || "amqps://byob:byob@mq.chills.co.za/byob"
 };

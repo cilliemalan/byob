4135483e14c18dedb51a967ada158beb86b0d420
Cillié Malan <cilliemalan@gmail.com>
2018-06-07 11:40:28 +0200

    Added messaging.js which manages messaging between nodes using rabbitmq

---
+++ messaging.js
@@ -0,0 +1,81 @@
+const EventEmitter = require('events');
+const amqp = require('amqplib');
+
+
+function encodeMessage(msg) {
+    return Buffer.from(JSON.stringify(msg));
+}
+
+
+// possible messages:
+// 1. tallest_block - request a response with the height of the tallest block and its hash
+// 2. block - a message containing a block
+// 3. transaction - a message containing a transaction
+// 4. chain_request - request to be broadcast a segment of the blockchain
+
+class MessagingClient extends EventEmitter {
+    constructor(identity, broker) {
+        super();
+
+        this.identity = identity;
+        this.broker = broker;
+    }
+
+    async connect() {
+        this.connection = await amqp.connect(this.broker);
+        this.channel = await this.connection.createChannel();
+        await this.channel.assertExchange('broadcast', 'fanout');
+        await this.channel.assertQueue(this.identity);
+        await this.channel.bindQueue(this.identity, 'broadcast', '');
+        await this.channel.consume(this.identity, ({ content, fields, properties }) => {
+            let decoded;
+            try {
+                decoded = JSON.parse(content.toString());
+            } catch (e) {
+                console.error('received invalid message on queue')
+            }
+
+            if (typeof decoded == "object") {
+                const { type, message, from } = decoded;
+                if (type && (typeof message !== "undefined") && from) {
+                    this.emit(type, message, from);
+                }
+            }
+        }, { noAck: true });
+    }
+
+    async broadcast(type, message = {}) {
+        await this.channel.publish('broadcast', '', encodeMessage({ from: this.identity, type, message }));
+    }
+
+    async send(recipient, type, message = {}) {
+        await this.channel.sendToQueue(recipient, encodeMessage({ from: this.identity, type, message }));
+    }
+
+    async broadcast_block(block) {
+        await this.broadcast('block', block);
+    }
+
+    async broadcast_transaction(transaction) {
+        await this.broadcast('transaction', transaction);
+    }
+
+    async send_highest(to, height) {
+        await this.send(to, 'highest', height);
+    }
+
+    async send_chain(to, chain) {
+        for (let i = 0; i < chain.length; i++) {
+            await this.send(to, 'block', chain[i]);
+        }
+    }
+
+    async send_chain_request(to) {
+        await this.send(to, 'request_chain');
+    }
+}
+
+
+module.exports = {
+    MessagingClient
+};
